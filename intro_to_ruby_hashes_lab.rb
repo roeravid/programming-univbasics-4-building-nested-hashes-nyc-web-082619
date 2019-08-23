2d59f133b2d9a614026fe48c27eb08709c73fc27
Joe David <javid92@gmail.com>
2019-08-23 18:08:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars =>
       {
-        :one_piece_owned => {},
+        :one_piece_owned => 25,
         :kjsg=> {},
         :lhjfsg=>{},
         :kjnskj=>{}
